51f085333cb0300ed88799a6f50e4c383c9fdce8
gabezxg <gabrielivanildo2009@gmail.com>
2025-11-06 16:05:13 -0300

    Second Commit

---
+++ resources/views/home_screen.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>A Lenda de Orion</title>
+</head>
+<style>
+    h1 {
+        text-align: center;
+        font-family: Arial;
+        color: purple;
+        font-size: 80px;
+    }
+    </style>
+<body>
+    <h1> A Lenda de Orion </h1>
+</body>
+</html>
