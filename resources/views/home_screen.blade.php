68a2a0b7bf39ae7d257ad8a2085fab503e6880b2
gabezxg <gabrielivanildo2009@gmail.com>
2025-11-14 12:16:53 -0300

    Fourth Commit

--- resources/views/home_screen.blade.php
+++ resources/views/home_screen.blade.php
@@ -3,15 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jogo RPG</title>
+    <title>A Lenda de Orion</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titan+One&family=Ultra&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <h1>A Lenda de Orion</h1>
-    <h2>Está pronto para se aventurar?</h2>
-    <p class="descricao">Seja bem-vindo ao Jogo RPG!</p>
-        <a class="button1" href="#">Novo Jogo</a>
-        <a class="button2" href="#">Continuar Jogo</a>
-        <a class="button3" href="#">Configurações</a>
+    <h2>Está pronto para se aventurar?</h2><br><br>
+    <p class="descricao">Seja bem-vindo ao Jogo RPG!</p><br><br>
+        <a class="button1" href="{{ route('Game') }}">Jogar</a>
+        <a class="button2" href="#">Configurações</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home_screen');
 });
+
+Route::get('/new-game', function () {
+    return view('new_game'); 
+})->name('Game');
