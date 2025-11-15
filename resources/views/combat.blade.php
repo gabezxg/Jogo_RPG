1504d1961502f6d49fa39253f6761da5f831f0c9
gabezxg <gabrielivanildo2009@gmail.com>
2025-11-15 12:40:24 -0300

    Sixth Commit

--- resources/views/combat.blade.php
+++ resources/views/combat.blade.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <title>Combate</title>
     <link rel="stylesheet" href="{{ asset('css/combat.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Titan+One&family=Ultra&display=swap" rel="stylesheet">
 </head>
 <body>
+    <div class="box1"> Jogador
+    </div>
+    <div class="box2"> Ogro
+    </div>
 </body>
-</html>
+</html> 
